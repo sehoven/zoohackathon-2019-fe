7f54232b6c6ece9663393a0eb186b3c24f86a0b0
asnewman <ashznewman@gmail.com>
2019-11-10 08:37:33 -0800

    Added minor ui tweaks

--- src/components/TopicInput.tsx
+++ src/components/TopicInput.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { FormControl, InputLabel, Select, Input, MenuItem, Chip } from '@material-ui/core'; 
+import styled from 'styled-components';
+import { FormControl, InputLabel, Select, Input, MenuItem, Chip } from '@material-ui/core';
 
 const topics = [
     'Topic 1',
@@ -9,39 +10,45 @@ const topics = [
     'Topic 5'
 ];
 
+const TopicInputDiv = styled.div`
+    margin-bottom: 10px;
+`;
+
 const TopicInput = () => {
     const [topicName, setTopicName] = React.useState<string[]>([]);
 
     const handleChange = (e: any) => {
         setTopicName(e.target.value as string[]);
     };
-      
+
     return (
-        <FormControl fullWidth>
-            <InputLabel id="mutiple-chip-label">Topics</InputLabel>
-            <Select
-                autoWidth
-                labelId="mutiple-chip-label"
-                id="mutiple-chip"
-                multiple
-                value={topicName}
-                onChange={handleChange}
-                input={<Input id="select-multiple-chip" />}
-                renderValue={selected => (
-                    <div>
-                        {(selected as string[]).map(value => (
-                            <Chip style={{margin: '5px'}} key={value} label={value} />
-                        ))}
-                    </div>
-                )}
+        <TopicInputDiv>
+            <FormControl fullWidth>
+                <InputLabel id="mutiple-chip-label">Topics</InputLabel>
+                <Select
+                    autoWidth
+                    labelId="mutiple-chip-label"
+                    id="mutiple-chip"
+                    multiple
+                    value={topicName}
+                    onChange={handleChange}
+                    input={<Input id="select-multiple-chip" />}
+                    renderValue={selected => (
+                        <div>
+                            {(selected as string[]).map(value => (
+                                <Chip style={{ margin: '5px' }} key={value} label={value} />
+                            ))}
+                        </div>
+                    )}
                 >
-                {topics.map(topic => (
-                    <MenuItem key={topic} value={topic}>
-                        {topic}
-                    </MenuItem>
-                ))}
-            </Select>
-        </FormControl>
+                    {topics.map(topic => (
+                        <MenuItem key={topic} value={topic}>
+                            {topic}
+                        </MenuItem>
+                    ))}
+                </Select>
+            </FormControl>
+        </TopicInputDiv>
     )
 }
 
